f4bed66f2e4ecaa6ff51770d7060c08f2ddb0dc1
evgeniya-egupova <evgeniya.egupova@intel.com>
2022-03-07 05:45:28 +0300

    add ohe train code

--- model/model.py
+++ model/model.py
@@ -1,9 +1,11 @@
 import os
 
 from pyspark.ml.feature import StringIndexer
+from pyspark.ml.feature import OneHotEncoder, OneHotEncoderModel
 
 LR_MODEL_PATH = 'lr_model'
 STRING_INDEXER_PATH = 'stringIndexer'
+OHE_GOT_ENCODER_PATH = 'ohe'
 
 
 def train_string_indexer(df, inputCols, outputCols, save_path='./', transform=True):
@@ -13,9 +15,23 @@ def train_string_indexer(df, inputCols, outputCols, save_path='./', transform=Tr
     string_indexer_model = string_indexer.fit(df)
     if transform:
         df = string_indexer_model.transform(df)
-        df = df.drop(*["neighbourhood_group", 'neighbourhood', 'room_type'])
+        df = df.drop(*inputCols)
 
     stringIndexerPath = os.path.join(save_path, STRING_INDEXER_PATH)
     string_indexer_model.save(stringIndexerPath)
 
     return string_indexer_model, df
+
+
+def train_one_hot_encoder(df, inputCols, outputCols, save_path='./', transform=True):
+    ohe = OneHotEncoder(inputCols=inputCols,
+                        outputCols=outputCols)
+    ohe_model = ohe.fit(df)
+    if transform:
+        encoded = ohe_model.transform(df)
+        df = encoded.drop(*inputCols)
+
+    ohe_model_path = os.path.join(save_path, OHE_GOT_ENCODER_PATH)
+    ohe_model.save(ohe_model_path)
+
+    return ohe_model, df
